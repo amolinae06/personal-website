d0edcd835d1e4e9ef5a77464d5510753302a7831
amolinae06 <amolinaegames@gmail.com>
2023-11-25 13:23:20 -0600

    added more icons and rework some stuff

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,10 @@
-particlesJS.load('particles-js', 'assets/particles.json')
+particlesJS.load('particles-js','assets/particles.min.json')
+
+const typed = new Typed('#typed', {
+	strings: ["\"Man of few words, ^500 aren't you?\""],
+	typeSpeed: 60,
+	cursorChar: '|',
+	onComplete() {
+		typed.cursor.remove();
+      }
+  });
