6d763f7e9de079c9c9012a21b8bb2703e55c5702
amolinae06 <amolinaegames@gmail.com>
2023-11-27 23:19:19 -0600

    updated navbar with new sounds

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,20 @@
 particlesJS.load('particles-js','assets/particles.min.json')
 
 const typed = new Typed('#typed', {
-	strings: ["\"Man of few words, ^500 aren't you?\""],
+	strings: ["\"Man of few words, ^400 aren't you?\""],
 	typeSpeed: 60,
 	cursorChar: '|',
 	onComplete() {
 		typed.cursor.remove();
       }
-  });
+});
+
+function PlayRollSound(soundobj) {
+	var thissound = document.getElementById(soundobj);
+	thissound.play();
+}
+
+function PlayClickSound(soundobj) {
+	var thissound = document.getElementById(soundobj);
+	thissound.play();
+}
